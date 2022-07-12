24f30c9deb638a7a547fe24087650098373d9202
J <Guruprasad.J@ad.jktech.com>
2022-07-12 12:53:45 +0530

    added api and observable

--- src/app/service/idtvnewsapi.service.ts
+++ src/app/service/idtvnewsapi.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IdtvnewsapiService {
+  constructor(private _http: HttpClient) {}
 
-  constructor() { }
+  //topheadline URL
+  topHeadlineNews =
+    'https://api.thenewsapi.com/v1/news/headlines?locale=us&language=en&api_token=hSBJlB1U5X9f8GNn3JLnnqYfSUFiwu6ly070f8Q0';
+
+    tcHeadlines():Observable<any>{
+      return this._http.get(this.topHeadlineNews)
+    }
 }
