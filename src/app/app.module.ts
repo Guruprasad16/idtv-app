62feeaa700406d87f86f8301565b800d1a57684d
J <Guruprasad.J@ad.jktech.com>
2022-07-12 12:13:52 +0530

    added th component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopheadlineComponent } from './topheadline/topheadline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheadlineComponent
   ],
   imports: [
     BrowserModule,
