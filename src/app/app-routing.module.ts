3229141ad7ee62c6d7fc15992359e07118a239c5
J <Guruprasad.J@ad.jktech.com>
2022-07-19 11:56:19 +0530

    imported tech component in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopheadlineComponent } from './topheadline/topheadline.component';
+import { TechComponent } from './tech/tech.component';
 
-const routes: Routes = [{ path: '', component: TopheadlineComponent }];
+const routes: Routes = [
+  { path: '', component: TopheadlineComponent },
+  { path: 'tech', component: TechComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
