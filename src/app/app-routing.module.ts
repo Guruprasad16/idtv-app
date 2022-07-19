9fe1e8e175a42e14565b64044945bb369c2bfd0b
J <Guruprasad.J@ad.jktech.com>
2022-07-19 12:07:33 +0530

    imported business component to approuting.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopheadlineComponent } from './topheadline/topheadline.component';
 import { TechComponent } from './tech/tech.component';
+import { BusinessComponent } from './business/business.component';
 
 const routes: Routes = [
   { path: '', component: TopheadlineComponent },
   { path: 'tech', component: TechComponent },
+  { path: 'buss', component: BusinessComponent }
 ];
 
 @NgModule({
