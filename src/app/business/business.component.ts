c9a838abf17d72da285359dbb34aefd9c2e49a96
J <Guruprasad.J@ad.jktech.com>
2022-07-19 12:18:55 +0530

    imported idtvapi in business.component.html

--- src/app/business/business.component.ts
+++ src/app/business/business.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IdtvnewsapiService } from '../service/idtvnewsapi.service';
 
 @Component({
   selector: 'app-business',
   templateUrl: './business.component.html',
-  styleUrls: ['./business.component.css']
+  styleUrls: ['./business.component.css'],
 })
 export class BusinessComponent implements OnInit {
+  constructor(private api: IdtvnewsapiService) {}
 
-  constructor() { }
-
+  //businessnews data
+  businessNewsData: any = [];
   ngOnInit(): void {
+    this.api.tcBuss().subscribe((result) => {
+      console.log(result);
+      this.businessNewsData = result.data;
+    });
   }
-
 }
