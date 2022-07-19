2725c47ad586c797941bb3ccb9daf6c33c76ab07
J <Guruprasad.J@ad.jktech.com>
2022-07-19 11:42:56 +0530

    added tech api and method

--- src/app/service/idtvnewsapi.service.ts
+++ src/app/service/idtvnewsapi.service.ts
@@ -8,10 +8,19 @@ import { Observable } from 'rxjs';
 export class IdtvnewsapiService {
   constructor(private _http: HttpClient) {}
 
-  //topheadline URL
+  //topheadline API URL
   topHeadlineNews =
     ' https://api.thenewsapi.com/v1/news/top?api_token=hSBJlB1U5X9f8GNn3JLnnqYfSUFiwu6ly070f8Q0&locale=in&limit=50';
+
+  //tech news API URL
+  techNews =
+    ' https://api.thenewsapi.com/v1/news/top?api_token=hSBJlB1U5X9f8GNn3JLnnqYfSUFiwu6ly070f8Q0&locale=in&category=technology&limit=50';
+
   tcHeadlines(): Observable<any> {
     return this._http.get(this.topHeadlineNews);
+  }
+
+  tcTech(): Observable<any> {
+    return this._http.get(this.techNews);
   }
 }
