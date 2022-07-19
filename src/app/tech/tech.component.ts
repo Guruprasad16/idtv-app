94e2a94a89bbd328d0be9ce915144d148c74d593
J <Guruprasad.J@ad.jktech.com>
2022-07-19 11:50:47 +0530

    updated tech component.ts

--- src/app/tech/tech.component.ts
+++ src/app/tech/tech.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IdtvnewsapiService } from '../service/idtvnewsapi.service';
 
 @Component({
   selector: 'app-tech',
   templateUrl: './tech.component.html',
-  styleUrls: ['./tech.component.css']
+  styleUrls: ['./tech.component.css'],
 })
 export class TechComponent implements OnInit {
+  constructor(private api: IdtvnewsapiService) {}
 
-  constructor() { }
-
+  //technews data
+  techNewsData: any = [];
   ngOnInit(): void {
+    this.api.tcTech().subscribe((result) => {
+      console.log(result);
+      this.techNewsData = result.data;
+    });
   }
-
 }
